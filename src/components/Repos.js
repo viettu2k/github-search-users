be6d748a5dc0283ff147110a938445b0981471a2
viettu2k <doanviettu2k@gmail.com>
2021-09-05 16:29:01 +0700

    adding search component

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -21,8 +21,6 @@ const Repos = () => {
     return total;
   }, {});
 
-  console.log(languages);
-
   const mostUsed = Object.values(languages)
     .sort((a, b) => {
       return b.value - a.value;
--- src/context/context.js
+++ src/context/context.js
@@ -15,8 +15,31 @@ const GithubProvider = ({ children }) => {
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
 
+  // request loading
+  const [requests, setRequests] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // check rate
+  const checkRequests = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data;
+        setRequests(remaining);
+        if (remaining === 0) {
+          // throw an error
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  // error
+
+  useEffect(checkRequests, []);
+
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+    <GithubContext.Provider value={{ githubUser, repos, followers, requests }}>
       {children}
     </GithubContext.Provider>
   );
