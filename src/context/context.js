c628fb8c0e0742b7bd95466e602236475aed6fd2
viettu2k <doanviettu2k@gmail.com>
2021-09-06 09:18:21 +0700

    handling rate limit error search component

--- src/context/context.js
+++ src/context/context.js
@@ -19,6 +19,9 @@ const GithubProvider = ({ children }) => {
   const [requests, setRequests] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
 
+  // error
+  const [error, setError] = useState({ show: false, msg: "" });
+
   // check rate
   const checkRequests = () => {
     axios(`${rootUrl}/rate_limit`)
@@ -26,20 +29,27 @@ const GithubProvider = ({ children }) => {
         let {
           rate: { remaining },
         } = data;
+        remaining = 0;
         setRequests(remaining);
         if (remaining === 0) {
-          // throw an error
+          toggleError(true, "sorry, you have exceeded your hourly rate limit!");
         }
       })
       .catch((err) => console.log(err));
   };
+
+  function toggleError(show = false, msg = "") {
+    setError({ show, msg });
+  }
 
   // error
 
   useEffect(checkRequests, []);
 
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers, requests }}>
+    <GithubContext.Provider
+      value={{ githubUser, repos, followers, requests, error }}
+    >
       {children}
     </GithubContext.Provider>
   );
