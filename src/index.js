d6f3ae02f3e45f6b00bd12e7dbc8b70efcbe6292
viettu2k <doanviettu2k@gmail.com>
2021-09-08 07:40:45 +0700

    adding login, logout

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Auth0Provider>
-      <GithubProvider
-        domain={process.env.REACT_APP_AUTH0_DOMAIN}
-        clientId={process.env.REACT_APP_AUTH0_CLIENT}
-        redirectUri={window.location.origin}
-      >
+    <Auth0Provider
+      domain={process.env.REACT_APP_AUTH0_DOMAIN}
+      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
         <App />
       </GithubProvider>
     </Auth0Provider>
