a1eb8461fac136621f3e9e750b56fbc56162eea4
viettu2k <doanviettu2k@gmail.com>
2021-09-08 08:12:09 +0700

    adding  private route and local storage

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ ReactDOM.render(
       domain={process.env.REACT_APP_AUTH0_DOMAIN}
       clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
       redirectUri={window.location.origin}
+      cacheLocation="localstorage"
     >
       <GithubProvider>
         <App />
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,17 +2,23 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import styled from "styled-components";
 import loginImg from "../images/login-img.svg";
+
 const Login = () => {
+  const { loginWithRedirect } = useAuth0();
+
   return (
     <Wrapper>
       <div className="container">
         <img src={loginImg} alt="github user" />
         <h1>github user</h1>
-        <button className="btn">login</button>
+        <button className="btn" onClick={() => loginWithRedirect()}>
+          login / sign up
+        </button>
       </div>
     </Wrapper>
   );
 };
+
 const Wrapper = styled.section`
   min-height: 100vh;
   display: grid;
